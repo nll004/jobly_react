f6f501ecf689b23050617f72fcbfe75cbdf5023a
nll004 <loninath17@gmail.com>
2023-01-12 14:25:12 -0500

    navbar styling

--- src/routes-nav/NavBar.js
+++ src/routes-nav/NavBar.js
@@ -2,6 +2,7 @@ import React, {useContext} from "react";
 import { NavLink, Link } from 'react-router-dom';
 import UserContext from "../context-hooks/UserContext";
 import AuthFuncContext from "../context-hooks/AuthFuncContext";
+import "./NavBar.css";
 
 function Nav(){
     const {currentUser} = useContext(UserContext);
@@ -9,17 +10,33 @@ function Nav(){
 
     return (
         <nav>
-            <Link to='/'> Jobly </Link>
-            <ul>
-                <NavLink to='/jobs'> Jobs </NavLink>
-                <NavLink to='/companies'> Companies </NavLink>
-                {currentUser && <>
-                    <NavLink to={`/profile/${currentUser.username}`}> Profile </NavLink>
-                    <button onClick={userLogout}> Logout </button>
+            <Link   to='/'
+                    id='navbar-logo'>
+                Jobly
+            </Link>
+            <ul className='navlink-container'>
+                <NavLink to='/jobs'
+                         className='navlink'>
+                    Jobs
+                </NavLink>
+                <NavLink to='/companies'
+                         className='navlink'>
+                    Companies
+                </NavLink>
+            {currentUser && <>
+                <NavLink to={`/profile/${currentUser.username}`}
+                         className='navlink'>
+                    Profile
+                </NavLink>
+                <button  onClick={userLogout}
+                         className="navlink"
+                         id='navbar-logout-btn' >
+                    Logout
+                </button>
                 </>}
             </ul>
         </nav>
     )
-}
+};
 
 export default Nav;
